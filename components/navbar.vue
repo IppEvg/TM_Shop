5df98194e75fdac971878eb3b9ce5bd4b8940620
IppEvg <ippevg@mail.ru>
2024-05-27 19:31:42 +0300

    ReductSite

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,13 +1,29 @@
 <template>
     <div class="wrapperNav">
-        <NuxtLink>logo</NuxtLink>
+        <div class="wrapperNaw_logo">
+            <NuxtLink :to="'/'"><img src="https://ippevg.github.io/PhServer/Assets/LogoTM.png" class="wrapperNaw_logo_image" alt="Logo"></NuxtLink>
+        </div>    
         <ul>
             <li v-for="(item,index) in li" :key="index">
                 <NuxtLink :to="item.to">{{ item.label }}</NuxtLink>           
             </li>
         </ul>
-        <div class="CheckInBlock">
-            
+        <div class="checkInBlock">
+            <div class="checkInBlock_items">
+                <NuxtLink :to="'#'">
+                    <img src="https://ippevg.github.io/PhServer/Assets/iconsHeart.png" class="checkInBlock_items_item" alt="heart">
+                </NuxtLink>
+            </div>
+            <div class="checkInBlock_items">
+                <NuxtLink :to="'#'">
+                    <img src="https://ippevg.github.io/PhServer/Assets/iconsBag.png" class="checkInBlock_items_item" alt="bag">
+                </NuxtLink>
+            </div>
+            <div class="checkInBlock_items">
+                <NuxtLink :to="'#'">
+                    <img src="https://ippevg.github.io/PhServer/Assets/iconsBurgerMenu.png" class="checkInBlock_items_item" alt="burger">
+                </NuxtLink>
+            </div>
         </div>
     </div>
     
@@ -31,9 +47,32 @@ import { ref } from 'vue'
 
 <style scoped lang="scss">
 .wrapperNav{
+    display: grid;
+    grid-template-rows: 80px;
+    grid-template-columns: 1fr 9fr 3fr;
+    margin: 30px auto ;
+    max-height: 80px;
+    .wrapperNaw_logo{
+        max-width: 100%;
+
+    }
+}
+.checkInBlock{
+    max-width: max-content;
     display: flex;
-    justify-content: space-between;
+    flex-direction: row;
     align-items: center;
+    justify-content: center;
+    gap: 4%;
+    height: 100%;
+    .checkInBlock_items{
+        max-width: 10%;
+        .checkInBlock_items_item{
+            width: 100%;
+            padding: 10%;
+            box-sizing: border-box;
+        }
+    }
 }
 ul{
     display: flex;
@@ -43,7 +82,7 @@ ul{
     list-style: none;
     padding: 0;
     li{
-        padding: 10px;
+        padding: 20px;
         gap: 20px;
     }
 }
